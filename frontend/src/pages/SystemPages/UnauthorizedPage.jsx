eb46395559e8efe3681359482f71da60d8e20057
Khánh Duy Phạm <khanhduy123qwe@gmail.com>
2026-06-11 11:35:07 +0700

    feat(system): add systemPages and related components

--- frontend/src/pages/SystemPages/UnauthorizedPage.jsx
+++ frontend/src/pages/SystemPages/UnauthorizedPage.jsx
@@ -1,5 +1,36 @@
+import { Link } from "react-router-dom";
+import { FaHome, FaLock } from "react-icons/fa";
 import "./UnauthorizedPage.scss";
+import { IoReload } from "react-icons/io5";
+import { BiLogIn } from "react-icons/bi";
 
-const UnauthorizedPage = () => {};
+const UnauthorizedPage = () => {
+  return (
+    <div className="unauthorized-container">
+      <div className="unauthorized-content">
+        {/* Grass Decoration */}
+        <div className="grass-decoration">
+          <div className="grass"></div>
+          <div className="grass"></div>
+          <div className="grass"></div>
+        </div>
+
+        {/* Error Text */}
+        <h1 className="error-number">403</h1>
+        <p className="error-description">
+          Xin lỗi, bạn không có quyền truy cập trang này.
+        </p>
+
+        {/* Action Button */}
+        <Link to={"/"} className="btn-home m-3">
+          <FaHome />
+        </Link>
+        <Link to={"/auth/login"} className="btn-home m-3">
+          <BiLogIn />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default UnauthorizedPage;

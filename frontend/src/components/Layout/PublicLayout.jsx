9d82c63c3e4484f78273aed9e26be5540f22730a
simonthuandev-cachy <nguyenducthuank588@gmail.com>
2026-06-15 22:21:28 +0700

    chore: customize the appearance of PublicLayout and ProtectedLayout components

--- frontend/src/components/Layout/PublicLayout.jsx
+++ frontend/src/components/Layout/PublicLayout.jsx
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router";
+import NavBar from "../NavBar";
+import Footer from "../Footer";
+import Chatbot from "../Common/Chatbot";
+import ScrollToTopButton from "../Common/ScrollToTopButton";
 
 const PublicLayout = () => {
   return (
     <>
+      <NavBar />
       <main>
         <Outlet />
       </main>
+      <Footer />
+
+      <ScrollToTopButton />
+      <Chatbot />
     </>
   );
 };
